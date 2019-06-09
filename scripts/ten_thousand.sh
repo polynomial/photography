0e6c44ec295b2c18a9402b84b0c2bf474c20844b
Benjamin Smith <polynomial@gmail.com>
2019-06-09 19:41:45 +0000

    create img movies too

--- scripts/ten_thousand.sh
+++ scripts/ten_thousand.sh
@@ -9,16 +9,17 @@ $echo mencoder "mf://IMG_*.JPG" -o IMG.mpg -ovc copy -oac copy &
 for b in `seq 0 9`; do
   for n in `seq 0 9`; do
     for y in `seq 0 9`; do
-      (
-      echo $n $y
-      $echo cp $(\ls IMG_${b}${n}${y}* | head -1) ${prefix}_${b}_${n}_${y}.jpg 
-      for i in IMG_${b}${n}${y}* ; do
-        echo $i
-        $echo convert ${prefix}_${b}_${n}_${y}.jpg $i -gravity center -compose lighten -composite -format jpg ${prefix}_${b}_${n}_${y}.jpg 
+      for z in `seq 0 9`; do
+        (
+        $echo cp $(\ls IMG_${b}${n}${y}${z}* | head -1) ${prefix}_${b}_${n}_${y}_${z}.jpg 
+        for i in IMG_${b}${n}${y}${z}* ; do
+          echo $i
+          $echo convert ${prefix}_${b}_${n}_${y}_${z}.jpg $i -gravity center -compose lighten -composite -format jpg ${prefix}_${b}_${n}_${y}_${z}.jpg
+        done
+        ) &
       done
-      ) &
+      wait
     done
-    wait
   done
 done
 $echo mencoder "mf://${prefix}_*" -o ${prefix}.avi -ovc lavc -lavcopts vcodec=mjpeg &
@@ -27,7 +28,6 @@ $echo cp $(\ls IMG_* | head -1) ${prefix}.jpg
 for i in ${prefix}_*; do
   $echo convert ${prefix}.jpg $i -gravity center -compose lighten -composite -format jpg ${prefix}.jpg
 done
-wait
 tprefix=trail_night
 source_prefix=night_auto_iso
 count=$(\ls -1 ${source_prefix}_* | grep -c .)
