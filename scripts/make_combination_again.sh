9e78fe9eaf185ed73e7eba0209322ca81a501357
Benjamin Smith <polynomial@gmail.com>
2019-08-30 15:54:50 -0700

    fix combination again

--- scripts/make_combination_again.sh
+++ scripts/make_combination_again.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 prefix=$1
-cp $(ls ${prefix}_* | head -1) ${prefix}.jpg
-for i in ${prefix}_*; do   
+cp $(ls ${prefix}_*.[Jj][Pp][Gg] | head -1) ${prefix}.jpg
+for i in ${prefix}_*.[Jj][Pp][Gg] ; do   
   $echo convert ${prefix}.jpg $i -gravity center -compose lighten -composite -format jpg ${prefix}.jpg
 done
 
